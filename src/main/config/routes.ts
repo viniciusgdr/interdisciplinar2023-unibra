eb32a95030d7ee74c9b1ca89a8becd0600e6a3c7
Vinicius Silva <viniciusvinisilva1@outlook.com>
2023-05-04 21:21:26 -0300

    fix prod

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,17 +1,21 @@
 import { Router, type Express } from 'express'
 import FastGlob from 'fast-glob'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
 export default (app: Express): void => {
   const router = Router()
   const routerHome = Router()
   app.use('/', router)
   app.use('/', routerHome)
   FastGlob.sync([
-    '**/src/main/routes/**-routes.ts',
-    '**/src/main/routes/**/**.ts'
+    process.env.PROD ? '**/dist/main/routes/**-routes.ts' : '**/src/main/routes/**-routes.ts',
+    process.env.PROD ? '**/dist/main/routes/**/**.ts' : '**/src/main/routes/**/**.ts'
   ]).map(async file => {
     (await import(`../../../${file}`)).default(router)
   })
-  FastGlob.sync('**/src/main/routes/**-home.ts').map(async file => {
+  FastGlob.sync([
+    process.env.PROD ? '**/dist/main/routes/**-home.ts' : '**/src/main/routes/**-home.ts'
+  ]).map(async file => {
     (await import(`../../../${file}`)).default(routerHome)
   })
 }
